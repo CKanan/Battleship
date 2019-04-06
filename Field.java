3ac6cc800e628a4b155dae2bda60ed7c57b2e7c1
CKanan <kenan.ceferli@yahoo.com>
2019-04-06 17:07:01 +0400

    modified field

--- Field.java
+++ Field.java
@@ -17,92 +17,86 @@ public class Field {
 		Position pos=new Position();
 		int size=0;
 		char orient=0;
-		boolean stat;
-		boolean status;
+		boolean stat,status,st;
 		
 		Scanner input = new Scanner(System.in);
 		for(int i=0;i<maxShip;i++) {
-			status=true;
-			while(status) {
-				stat=true;
-				while(stat) {
-					System.out.println("Give ship size: "); 
-					size=input.nextInt();
-					if(size<2 || size>5 || nSize[size-2]==0) {
-						System.out.println("Wrong size or inappropriate input!");
+			st=true;
+			while(st) {
+				status=true;
+				while(status) {
+					stat=true;
+					while(stat) {
+						System.out.println("Give "+(i+1)+" ship size: "); 
+						size=input.nextInt();
+						if(size<2 || size>5 || nSize[size-2]==0) {
+							System.out.println("Wrong size or inappropriate input!\nTry Again");
+						}
+						else {
+							nSize[size-2]=nSize[size-2]-1;
+							stat=false;
+						}
 					}
-					else {
-						nSize[size-2]=nSize[size-2]-1;
-						stat=false;
+					stat=true;	
+					while(stat) {
+						System.out.println("Give beginnig position of ship: ");
+						int x=input.nextInt();
+						int y=input.nextInt();
+						pos=new Position(x, y);
+						if(!pos.inBorder()) 
+							System.out.println("Positions have to between [0,10]!");
+						stat=!pos.inBorder();
 					}
+					stat=true;
+					while(stat) {
+						System.out.println("Give orientation: "); 
+						orient=input.next().charAt(0);
+						if(orient=='v' || orient=='V' || orient=='h' || orient=='H')
+							stat=false;
+						else System.out.println("Orientation have to (v,V or h,H)");
+					}	
+					
+					ships[i]=new Ship(pos, size, orient);
+
+					status=isCrashed(pos, size, orient,i);
 				}
-				stat=true;	
-				while(stat) {
-					System.out.println("Give beginnig position of ship: ");
-					int x=input.nextInt();
-					int y=input.nextInt();
-					pos=new Position(x, y);
-					stat=!pos.inBorder();
-				}
-				stat=true;
-				while(stat) {
-					System.out.println("Give orientation: "); 
-					orient=input.next().charAt(0);
-					if(orient=='v' || orient=='V' || orient=='h' || orient=='H')
-						stat=false;
-				}	
-				
-				ships[i]=new Ship(pos, size, orient);
-				
-				status=isCrashed(pos, size, orient,i);
-			}	
-//				Ship compareShip=ships[0];
-//				if(i>0) {
-//					for(int k=0;k<compareShip.getSize();k++) {
-//						for(int j=0;j<ships[i].getSize();i++) {
-//							if(compareShip.getPosition()[k].x==ships[i].getPosition()[j].x && compareShip.getPosition()[k].y==ships[i].getPosition()[j].y) {
-//							 	System.out.println("Ships are intersected\n"); 
-//							 	i=i-1;
-//							 	isCrashed=true;
-//							}
-//							else {
-//								compareShip=ships[i];
-//								isCrashed=false;
-//							}
-//							
-//						}
-//					}
-//				}
-//			}				
-		}
-		for (int i = 0; i < 10; ++i) System.out.println();
+				st=inOut(ships[i],size);
+			}
+		}	
+		for (int i = 0; i < 50; ++i) System.out.println();
 	}
 	
-	public boolean isCrashed(Position position,int size,char orient,int maxShip) {
-		for(int i=0;i<maxShip;i++) {
+	public boolean isCrashed(Position position,int size,char orient,int maxship) {
+		for(int i=0;i<maxship;i++) {
 			for(int j=0;j<size;j++) {
 				for(int k=0;k<ships[i].getSize();k++) {
-					if(orient=='v' || orient=='V') {
-						if(position.x==ships[i].getPosition()[k].x && position.y==ships[i].getPosition()[k].y+j) {
-							System.out.println("Ships are intersect");
+					if(orient=='v' || orient=='V' || orient=='H' || orient=='h') {
+						if((position.x==ships[i].getPosition()[k].x && position.y+j==ships[i].getPosition()[k].y) || (position.x+j==ships[i].getPosition()[k].x && position.y==ships[i].getPosition()[k].y)) {
+							System.out.println("Ships are intersect\nTry Again!");
+							nSize[size-2]=nSize[size-2]+1;
 							return true;
 						}
 					}
-					if(orient == 'H' || orient=='h') {
-						if(position.x==ships[i].getPosition()[k].x+j && position.y==ships[i].getPosition()[k].y) {
-							System.out.println("Ships are intersect");
-							return true;
-						}
-					} 
 				}
 			}
 		}	
 		return false;
 	}
 	
+	public boolean inOut(Ship ship, int size) {
+		for(int i=0;i<ship.getSize();i++) {
+			if(!ship.getPosition()[i].inBorder()) { 
+				nSize[size-2]=nSize[size-2]+1;
+				System.out.println("Ship is in out!\nTry again");
+				return true;
+			}
+		}
+		return false;
+	}
+	
 	public int isFired(Position pos) {
 		if(!pos.inBorder()) {
-			System.out.println("Positions are in out!!");
+			System.out.println("Position is in out!");
 			return 0;
 		}
 		if(isShooted[column*pos.y+pos.x]!=0 && isShooted[column*pos.y+pos.x]!=3){
