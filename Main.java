8a08444556e781b517d17986bac7363606fc5a71
CKanan <kenan.ceferli@yahoo.com>
2019-04-18 14:57:15 +0400

    created bot

--- Main.java
+++ Main.java
@@ -1,24 +1,32 @@
 package battleship;
 
+import java.util.Scanner;
+
 public class Main {
 	
 	public static void main(String[] args) {
 	
 		System.out.println("Welcome to Naval Battleship!\n");
-	
-//		User Kanan=new User("Kanan");
-//		User Nazir=new User("Nazir");
-//		
-//		while(true) {
-//			if(Kanan.play(Nazir)) break;
-//			if(Nazir.play(Kanan)) break;
-//		}
-		User Kanan=new User("Kanan");
-		User BOT=new User("BOT");
 		
-		while(true) {
-			if(Kanan.play(BOT)) break;
-			if(BOT.play(Kanan)) break;
+		User Kanan=new User("Kanan");
+		
+		System.out.println("Choose your opponent options:\ntrue-for USER,false-for BOT");
+		Scanner sc = new Scanner(System.in);
+		boolean opponent=sc.nextBoolean();
+	
+		if(opponent) {
+			User Nazir=new User("Nazir");
+			while(true) {
+				if(Kanan.play(Nazir)) break;
+				if(Nazir.play(Kanan)) break;
+			}
+		}
+		else {
+			User BOT=new User("BOT");
+			while(true) {
+				if(Kanan.play(BOT)) break;
+				if(BOT.play(Kanan)) break;
+			}
 		}
 	}
 
